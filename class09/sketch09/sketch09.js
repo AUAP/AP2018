627bf633e90a7aab3d11d26256ed3a8aaeca33af
Winnie Soon <siusoon@gmail.com>
2018-03-26 17:42:02 +0200

    Update sketch09.js

--- class09/sketch09/sketch09.js
+++ class09/sketch09/sketch09.js
@@ -37,7 +37,7 @@ function draw() {
 			img_x = floor(random(0,img.width-1));
 			img_y = floor(random(0,img.height-1));
 			loc = (img_x+img_y * img.width)*4;		//which pixel of the image (and each pixel array holds red, green, blue and alpha values)
-			stroke(color(img.pixels[loc],img.pixels[loc + 1], img.pixels[loc+2]));
+			stroke(color(img.pixels[loc],img.pixels[loc + 1], img.pixels[loc+2]));  //rgb values
 			line(img_x,0,img_x,height);
 			*/
 		});
